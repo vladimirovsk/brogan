467300f69e922ddc4ffc59dfa711bfefa2bb0fcb
vladimirovsk <vladimirovsk@gmail.com>
2021-02-01 14:56:13 +0100

    master

--- src/component/galeria/Galeria.js
+++ src/component/galeria/Galeria.js
@@ -3,9 +3,12 @@ import {GridList, GridListTile, GridListTileBar, ListSubheader, IconButton} from
 import ApartmentIcon from '@material-ui/icons/Apartment';
 import { makeStyles } from '@material-ui/core/styles';
 import Tooltip from '@material-ui/core/Tooltip';
+import {Button} from '@material-ui/core';
 import Footer from '../footer/Footer';
 import GaleriaDialog from './GaleriaDialog'
 import ProjectData from './ProjectData'
+
+import app from "firebase/app";
 
 
 
@@ -50,11 +53,44 @@ function Galeria() {
     },
   }));
 
-  function BootstrapTooltip(props) {
+  function  BootstrapTooltip(props) {
     const classes = useStylesBootstrap();
     return <Tooltip arrow classes={classes} {...props} />;
   }
+ 
+  
+  const firebaseConfig = {
 
+    // apiKey: "AIzaSyBtzQnv3BCDOk-WCZ6uTSUiowCKwisJidk",
+    // authDomain: "brogan-c0f45.firebaseapp.com",
+    // databaseURL: "https://brogan-c0f45-default-rtdb.europe-west1.firebasedatabase.app",
+    // projectId: "brogan-c0f45",
+    // storageBucket: "brogan-c0f45.appspot.com",
+    // messagingSenderId: "502624265935",
+    // appId: "1:502624265935:web:c7afdeb28cd63a3458f9ba",
+    // measurementId: "G-JEWZCWBJMW"
+
+    appName: "brogan-c0f45",
+    apiKey: process.env.REACT_APP_FIRBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIRBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIRBASE_ID,
+    storageBucket: process.env.REACT_APP_FIRBASE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIRBASE_SENDER_ID ,
+    appId: process.env.REACT_APP_FIRBASE_API_APP_ID,
+    measurementId: process.env.REACT_APP_FIRBASE_MEAS_ID 
+  };
+
+  let myApp;
+  React.useEffect(()=>{
+    myApp = app.initializeApp(firebaseConfig);
+
+  }, [])
+  async function handleFirebaseConnect ( ) {
+
+
+     console.log(myApp);
+  }
+    
   
   return (
     <div className={classes.root}>
@@ -63,6 +99,7 @@ function Galeria() {
         <GridListTile key="Subheader" cols={3} style={{ height: 'auto',  }}>
           <ListSubheader component="div" style={{color: 'white', fontSize: '2em', fontFamily:"'Fredoka One', cursive" }}>GALERIA
           <h1 style={{color:'green'}}>{myVar}</h1>
+          <Button variant="contained" color="primary" onClick={handleFirebaseConnect}>TEST FIREBASE</Button>
           </ListSubheader>
         </GridListTile>
         {ProjectData.map((project) => (
